7ce26ef0e04118fbb769616be9c99955f0419fcc
Szymon Soczewka <42994488+Sajmonoo1998@users.noreply.github.com>
2020-04-27 13:18:05 +0200

    icon

--- app/src/main/java/team12/workoutmadness/MainActivity.java
+++ app/src/main/java/team12/workoutmadness/MainActivity.java
@@ -1,14 +1,17 @@
 package team12.workoutmadness;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
+
 }

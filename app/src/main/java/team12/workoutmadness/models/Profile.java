b2ec8bc3023ad52aa5628f8bc4884caced516019
Máté <34075771+leopoldus97@users.noreply.github.com>
2020-04-28 15:54:08 +0200

    NumberPicker, BMI calculation
    NumberPicker implemented for height, weight and age
    BMI calculated automatically as the user changes either their height or weight

---
+++ app/src/main/java/team12/workoutmadness/models/Profile.java
@@ -0,0 +1,46 @@
+package team12.workoutmadness.models;
+
+public class Profile {
+    private String id, name;
+    private int height, weight, age;
+
+    public Profile(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
